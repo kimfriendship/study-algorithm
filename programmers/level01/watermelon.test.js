63687f8b71702af9b9a1401b728aa93de8ed405a
kimfriendship <kfriendship94@gmail.com>
2020-10-30 01:13:59 +0900

    :sparkles: Solve kth number

--- programmers/level01/watermelon.test.js
+++ programmers/level01/watermelon.test.js
@@ -1,7 +1,8 @@
 // 수박수박수박수박수?
 
 // 문제 설명
-// 길이가 n이고, 수박수박수박수....와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 예를들어 n이 4이면 수박수박을 리턴하고 3이라면 수박수를 리턴하면 됩니다.
+// 길이가 n이고, 수박수박수박수....와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요.
+// 예를들어 n이 4이면 수박수박을 리턴하고 3이라면 수박수를 리턴하면 됩니다.
 
 // 제한 조건
 // n은 길이 10,000이하인 자연수입니다.
@@ -15,7 +16,7 @@ function solution(n) {
   let answer = "";
 
   for (let i = 1; i <= n; i++) {
-    anwer += i % 2 ? "수" : "박";
+    answer += i % 2 ? "수" : "박";
   }
 
   return answer;
@@ -23,7 +24,7 @@ function solution(n) {
 
 describe("watermelon test", () => {
   it("test", () => {
-    expect(solution(3).toBe("수박수"));
-    expect(solution(4).toBe("수박수박"));
+    expect(solution(3)).toBe("수박수");
+    expect(solution(4)).toBe("수박수박");
   });
 });
