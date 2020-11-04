d7fd68e830ab806ddba6850651d70f3a53dc0474
kimfriendship <kfriendship94@gmail.com>
2020-11-05 00:48:37 +0900

    :sparkles: Implement stack

---
+++ udemy/data-structure/stack.test.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.size) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+
+    return ++this.size;
+  }
+
+  pop() {
+    if (!this.size) return null;
+
+    const temp = this.first;
+    this.first = temp.next;
+    if (!this.first) this.last = null;
+
+    this.size--;
+    return temp.val;
+  }
+}
