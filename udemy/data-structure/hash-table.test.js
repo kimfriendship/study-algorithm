57023353b3890b3ed69fd48d71171b318e1a4182
kimfriendship <kfriendship94@gmail.com>
2020-11-22 14:20:40 +0900

    :sparkles: Implement set and get of hash map

---
+++ udemy/data-structure/hash-table.test.js
@@ -0,0 +1,43 @@
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  hash(key) {
+    let total = 0;
+    let primeNum = 31;
+
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      const val = key[i].charCodeAt(0) - 96;
+      total = (total * primeNum + val) % this.keyMap.length;
+    }
+
+    return total;
+  }
+
+  set(key, val) {
+    const idx = this.hash(key);
+    if (!this.keyMap[idx]) this.keyMap[idx] = [[key, val]];
+    else this.keyMap[idx].push([key, val]);
+
+    return this;
+  }
+
+  get(key) {
+    const idx = this.hash(key);
+    if (this.keyMap[idx]) {
+      for (const pair of this.keyMap[idx]) {
+        if (pair[0] === key) return pair[1];
+      }
+    }
+
+    return undefined;
+  }
+}
+
+const hashMap = new HashTable(5);
+hashMap.set("wooey", 27);
+hashMap.set("eddie", 29);
+hashMap.set("eun", 22);
+hashMap.set("sol", 31);
+hashMap.set("claire", 26);
