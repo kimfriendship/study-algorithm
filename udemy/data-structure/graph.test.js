209daec8fef2057bca48e63ec9f21c498bb17767
kimfriendship <kfriendship94@gmail.com>
2020-11-23 21:53:07 +0900

    :sparkles: Implement DFS and BFS in graph

--- udemy/data-structure/graph.test.js
+++ udemy/data-structure/graph.test.js
@@ -26,11 +26,78 @@ class Graph {
       (key) => key !== vtx1
     );
   }
+
+  DFSRecursively(vertex) {
+    const results = [];
+    const visited = {};
+
+    const dfs = (v) => {
+      if (!v) return null;
+      visited[v] = true;
+      results.push(v);
+
+      this.adjacencyList[v].forEach((neighbor) => {
+        if (!visited[neighbor]) dfs(neighbor);
+      });
+    };
+
+    dfs(vertex);
+    return results;
+  }
+
+  DFSIteratively(vertex) {
+    const stack = [vertex];
+    const results = [];
+    const visited = { [vertex]: true };
+
+    while (vertex && stack.length) {
+      const v = stack.pop();
+      results.push(v);
+
+      this.adjacencyList[v].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+
+    return results;
+  }
+
+  BFS(vertex) {
+    const queue = [vertex];
+    const results = [];
+    const visited = { [vertex]: true };
+
+    while (vertex && queue.length) {
+      const v = queue.shift();
+      results.push(v);
+
+      this.adjacencyList[v].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+
+    return results;
+  }
 }
 
-const graph = new Graph();
-graph.addVertex("Seoul");
-graph.addVertex("Busan");
-graph.addVertex("Jeju");
-graph.addEdge("Seoul", "Jeju");
-graph.addEdge("Seoul", "Busan");
+const g = new Graph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
