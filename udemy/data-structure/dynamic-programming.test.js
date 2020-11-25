1dce70297ec78d9b5d7d4e07c60da360ac0ebd6a
kimfriendship <kfriendship94@gmail.com>
2020-11-25 16:53:05 +0900

    :sparkles: Implement fibonacci with memoization

--- udemy/data-structure/dynamic-programming.test.js
+++ udemy/data-structure/dynamic-programming.test.js
@@ -2,3 +2,10 @@ function fib(n) {
   if (n <= 2) return 1;
   return fib(n - 2) + fib(n - 1);
 }
+
+function memoFib(n, memo = [0, 1, 1]) {
+  if (memo[n]) return memo[n];
+  const result = memoFib(n - 2, memo) + memoFib(n - 1, memo);
+  memo[n] = result;
+  return result;
+}
